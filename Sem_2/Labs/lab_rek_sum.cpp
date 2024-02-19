8e17e4611a9d009e83eb8a903cbae7554815bae1
ksinph <146628117+ksinph@users.noreply.github.com>
2024-02-19 16:31:02 +0500

    Add files via upload

---
+++ Sem_2/Labs/lab_rek_sum.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int factorial(int n)
+{
+	if (n < 2) { return 1; }
+	return n * factorial(n - 1);
+}
+int fib(int n,int x)
+{
+	return 1+(pow(log(3),n)/factorial(n))*pow(x,n);
+}
+int main()
+{
+	int n,x;
+	cin >> n>>x;
+	cout << fib(n,x) << " ";
+	
+	return 0;
+}
