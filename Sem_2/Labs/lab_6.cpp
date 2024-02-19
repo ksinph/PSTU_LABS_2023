e109989335f34480f066875b8152964d8df9d957
ksinph <146628117+ksinph@users.noreply.github.com>
2024-02-19 14:48:46 +0500

    Add files via upload

---
+++ Sem_2/Labs/lab_6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+bool gets(string s)
+{
+    int len =s.length();
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    char s[255];
+    cin >> s;
+    if(gets(s))
+    {
+        cout << " является полиндромом";
+    }
+    else
+    {
+        cout << " не является полиндромом";
+    }
+}
