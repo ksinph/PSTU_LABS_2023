de75951d251d8c514a5f4c6d91afeb7bfdf07cae
ksinph <146628117+ksinph@users.noreply.github.com>
2024-02-19 16:27:36 +0500

    Add files via upload

---
+++ Sem_2/Labs/lab_rec_hanoi.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoi(int n,int start, int buf,int end)
+{
+    if (n>0)
+    {
+        hanoi(n - 1, start, buf, end);
+        cout << "from " << start << " to ---> " << end << endl;
+        hanoi(n - 1, buf, end, start);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    hanoi(n,1,2,3);
+    return 0;
+}
