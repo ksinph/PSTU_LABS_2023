f98c9ed31a5d45ff9c4ddf796d17f7cbddff9c87
ksinph <146628117+ksinph@users.noreply.github.com>
2024-02-19 15:03:35 +0500

    Add files via upload

---
+++ Sem_2/Labs/lab_7.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<complex>
+using namespace std;
+complex<double> sum(complex<double> a, complex<double> b) { return a + b; }
+int sum(int c, int w) { return c + w; }
+int main()
+{
+    int c, w;
+    cin >> c;
+    cin >> w;
+    double a1 = 10.20; double a2 = 10.23;
+    complex<double> a(a1, a2);
+    complex<double> b(a2, a1);
+    cout << sum(c, w);
+    cout << sum(a, b);
+}
