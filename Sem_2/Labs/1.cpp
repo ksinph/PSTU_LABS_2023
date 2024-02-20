d3e1b7f76a83a8a7abdc7f08fdc1ecd143d8f0e1
ksinph <146628117+ksinph@users.noreply.github.com>
2024-02-20 10:14:41 +0500

    Add files via upload

---
+++ Sem_2/Labs/1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int m, s = 1;
+    const int n = 100;
+    int a[n][n];
+    cin >> m;
+    cout << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if ((i + j) % 2 == 1)
+            {
+                a[i][j] = 0;
+            }
+            else
+            {
+                a[i][j] = s;
+                s++;
+                if (s == 10)
+                {
+                    s = 1;
+                }
+            }
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Sem_2/Labs/2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int m = 100;
+	int numbers, n;
+	int a[m][m] = { 0 };
+	cin >> n;
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		numbers = 1;
+		for (int j = n - 1; j >= 0; j--)
+		{
+			if (j <= i)
+			{
+				a[i][j] = numbers;
+				numbers++;
+				if (numbers == 10) numbers = 1;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
