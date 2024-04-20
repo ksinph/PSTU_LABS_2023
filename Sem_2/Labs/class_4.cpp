16a7b5a1364c11f68cb27025525930ed18691702
ksinph <146628117+ksinph@users.noreply.github.com>
2024-04-20 22:18:34 +0500

    Add files via upload

---
+++ Sem_2/Labs/class_4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Triad.h"
+using namespace std;
+int main ()
+{
+	Triad a;
+	cin >> a;
+	cout << a;
+	Triad b(4, 5, 6 );
+	cout << b;
+	a = b;
+	cout << a;
+	Time c;
+	cin >> c;
+	cout << c;
+
+	return 0;
+}
