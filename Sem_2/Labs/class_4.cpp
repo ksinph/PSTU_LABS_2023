608a0c14cb83ba753536261f31565abde5b7f3d3
ksinph <146628117+ksinph@users.noreply.github.com>
2024-04-21 15:08:41 +0500

    Add files via upload

---
+++ Sem_2/Labs/class_4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Triad.h"
+#include "Time.h"
+using namespace std;
+
+void f1(Triad& c)
+{
+	c.Set_first(9);
+	cout << c;
+	c.Set_second(89);
+	cout << c;
+	c.Set_third(23);
+	cout << c;
+}
+
+Triad f2()
+{
+	Time l(2, 3, 7, 8, 45, 56);
+	return l;
+}
+
+void main()
+{
+	Triad a;
+	cin >> a;
+	cout << a;
+	Time c;
+	cin >> c;
+	cout << c;
+	f1(c);
+	a = f2();
+	cout << a;
+	
+}
