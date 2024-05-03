4e466524f01e995b1e118adec3145c029e17d39d
ksinph <146628117+ksinph@users.noreply.github.com>
2024-05-03 21:21:09 +0500

    Update sortHoar.cpp

--- Sem_2/Labs/LAB_14/sortHoar.cpp
+++ Sem_2/Labs/LAB_14/sortHoar.cpp
@@ -1,64 +1,158 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
-const int kolvo = 25;
+const int cnt = 25;
 
 void element(int arr[])
 {
-    for (int i = 0; i < kolvo; i++)
+    for (int i = 0; i < cnt; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-
-int partition(int arr[], int low, int high)
+void TrueMerge(int* arr, int right, int left)
 {
-    int pivot = arr[low];
-    int num = 0;
-
-    for (int i = low + 1; i <= high; i++)
+    int PackOne = 1;
+    int PackTwo = 1;
+    int index = 0;
+    bool NeedOneMore = false;
+    while (arr[index] <= arr[index + 1] && index < right - 1) // Вычисление размера первой серии
     {
-        if (arr[i] < pivot) { num++; }
+        PackOne++;
+        index++;
     }
-
-    int pos = low + num;
-
-    swap(arr[pos], arr[low]);
-
-    int i = low, j = high;
-    while (i < pos && j > pos)
+    if (arr[index] > arr[index + 1] && index < right - 1) // Вычисление размера второй серии
     {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-
-        if (i < pos && j > pos)
+        index++;
+        NeedOneMore = true;
+        while (arr[index] <= arr[index + 1] && index < right - 1)
         {
-            swap(arr[i++], arr[j--]);
+            PackTwo++;
+            index++;
         }
     }
-    return pos;
+    int number = index + 1; // Number = индекс первого числа последовательности, не входящего в серию
+    int* farr = new int[PackOne]; // farr и sarr - серии. Далее они инициализируются
+    int* sarr = new int[PackTwo];
+    for (index = 0; index < PackOne; index++)
+    {
+        farr[index] = arr[index];
+    }
+    int tempindex = 0;
+    if (NeedOneMore)
+    {
+        for (index = PackOne; index < PackOne + PackTwo; index++)
+        {
+            sarr[tempindex] = arr[index];
+            tempindex++;
+        }
+    }
+    cout << endl;
+    cout << "1: ";
+    for (int i = 0; i < PackOne; i++)
+    {
+        cout << farr[i] << " ";
+    }
+    cout << endl << endl;
+    cout << "2: ";
+    for (int i = 0; i < PackTwo; i++)
+    {
+        cout << sarr[i] << " ";
+    }
+    cout << endl << endl;
+    int j = 0;
+    int l = 0;
+    if (NeedOneMore)
+    {
+        for (index = 0; index < number; index++)
+        {
+            if ((farr[l] < sarr[j] || j >= PackTwo) && l < PackOne)
+            {
+                arr[index] = farr[l];
+                l++;
+            }
+            else
+                if ((farr[l] > sarr[j] || l >= PackOne) && j < PackTwo)
+                {
+                    arr[index] = sarr[j];
+                    j++;
+                }
+                else
+                    if (farr[l] == sarr[j] && j < PackTwo && l < PackOne)
+                    {
+                        arr[index] = farr[l];
+                        arr[index + 1] = sarr[j];
+                        l++;
+                        j++;
+                        index++;
+                    }
+        }
+    }
 }
-void quickSort(int arr[], int low, int high)
+
+void multiPhaseSort(int arr[])
 {
-    if (low >= high) return;
+    bool swapped = true;
+    int gap = cnt;
+    while (gap > 1 || swapped)
+    {
+        gap = (gap * 10) / 13;
+        if (gap < 1)
+            gap = 1;
 
-    int pI = partition(arr, low, high);
-
-    // сортируем часть элементов до pivot
-    quickSort(arr, low, pI - 1);
-
-    // сортируем часть элементов после pivot
-    quickSort(arr, pI + 1, high);
+        swapped = false;
+        for (int i = 0; i < cnt - gap; i++)
+        {
+            if (arr[i] > arr[i + gap])
+            {
+                swap(arr[i], arr[i + gap]);
+                swapped = true;
+            }
+        }
+    }
 }
+
+void Sort(int* arr, int right, int left)
+{
+    if (left < right)
+    {
+        TrueMerge(arr, right, left);
+        Sort(arr, right, left + 1);
+    }
+}
+
+void PhaseSort(int* arr, int right, int left)
+{
+    if (left < right)
+    {
+        multiPhaseSort(arr);
+        PhaseSort(arr, right, left + 1);
+    }
+}
+
 int main()
 {
-    int arr[kolvo];
-    for (int i = 0; i < kolvo; i++) arr[i] = rand() % 10;
+    setlocale(LC_ALL, "Russian");
+    int arr[cnt];
+    for (int i = 0; i < cnt; i++) arr[i] = rand() % 10;
     element(arr);
     cout << endl;
-    quickSort(arr, 0, kolvo);
+    cout << "1 - естественная сортировка \n2 - многофазная сортировка \nВведите цифру которая бдует соответствовать сортировке которую нужно применить: ";
+    int n;
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        Sort(arr, cnt, 0);
+        break;
+    case 2:
+        PhaseSort(arr, cnt, 0);
+        break;
+    default:
+        cout << "Неправильный выбор";
+    }
+    cout << endl;
     element(arr);
     return 0;
 }
