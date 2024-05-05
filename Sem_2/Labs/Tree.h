69bef6c01f417a25f5d81c4840c320894eeb0973
ksinph <146628117+ksinph@users.noreply.github.com>
2024-05-05 22:52:11 +0500

    Add files via upload

---
+++ Sem_2/Labs/Tree.h
@@ -0,0 +1,25 @@
+template <class S>
+class SearchTree {
+public:
+	S data; // Данные типа Т
+	SearchTree* left; // Указатель на узел слева
+	SearchTree* right; // Указатель на узел справа
+	SearchTree* parent; // Указатель на предка
+	SearchTree(S); // Конструктор
+	~SearchTree(); // Деструктор
+	void deleteSearchTree() { delete this; }
+	// Удалить дерево
+	void printSearchTree(int);
+	// Горизонтальная печать дерева
+	void inOrder(SearchTree<S>*);
+	// Симметричный обход дерева
+	void setData(Sdt) { data = dt; }
+	// Установить данные для узла
+	SearchTree<S>* next(); // Найти следующий элемент
+	SearchTree<S>* prev(); // Найти предыдущий элемент
+	void insertNode(S); // Вставить узел
+	void deleteNode(S); // Удалить узел
+	SearchTree<S>* findElement(S); // Найти элемент
+	SearchTree<S>* findMax(); // Найти максимум
+	SearchTree<S>* findMin(); // Найти минимум
+};
