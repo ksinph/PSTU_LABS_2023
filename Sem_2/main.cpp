df68f2b513d3fe6046b51024cca78f48115cdd87
ksinph <146628117+ksinph@users.noreply.github.com>
2024-05-10 17:15:52 +0500

    Add files via upload

---
+++ Sem_2/main.cpp
@@ -0,0 +1,8 @@
+SearchTree<int>* tree = new SearchTree<int>(2);
+tree->insertNode(1);
+tree->insertNode(3);
+tree->insertNode(6);
+tree->insertNode(4);
+tree->insertNode(7);
+tree->insertNode(1);
+tree->printSearchTree(1);
