6c1cb2176755c7dd084a817cc38f6582ce1a0edf
ksinph <146628117+ksinph@users.noreply.github.com>
2024-05-12 16:11:39 +0500

    Add files via upload

---
+++ Sem_2/Labs/recursion/Tree.h
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+struct Node
+{
+	int data;
+	int counter;
+	Node* left, * right;
+};
+
+struct BinaryTree
+{
+	Node* root = NULL;
+};
+
+void AddToSubTree(Node*& node, Node* newnode);//функция поддерева, предаем ссылку на указатель    
+
+void AddToTree(BinaryTree& tree, int d);
+void PrintNode(Node* node);
+void PrintTree(BinaryTree tree);
+void PrintSubTreeStructure(Node* node, int depth, bool isroot = false);
+void PrintTreeStructure(BinaryTree tree);
+Node*  SearchInSubTree(Node* node, int d);
+Node*  SearchInTree(BinaryTree  tree, int d);
---
+++ Sem_2/Labs/recursion/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include "Tree.h"
+using namespace std;
+
+int main()
+{
+	BinaryTree tree;
+	int n;
+	scanf_s("%i", &n);
+	int d;
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%i", &d);
+		AddToTree(tree, d);
+	}
+	PrintTree(tree);
+	cout << endl;
+	cout << "Vvedite novoe chislo ";
+	cin >> d;
+	AddToTree(tree, d);
+	PrintTree(tree);
+	cout << endl;
+	scanf_s("%i", &d);
+	Node* res = SearchInTree(tree, d);
+	printf("This tree has number %i in quantility %i\n", d, res);
+	cout << endl;
+	PrintTreeStructure(tree);
+	return 0;
+}
