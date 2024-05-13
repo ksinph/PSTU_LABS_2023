dde8e8a8b0b411fec155fdb1f641c2b21ba6c7d6
ksinph <146628117+ksinph@users.noreply.github.com>
2024-05-13 20:51:54 +0500

    Delete Sem_2/main.cpp

--- Sem_2/main.cpp
+++
@@ -1,8 +0,0 @@
-SearchTree<int>* tree = new SearchTree<int>(2);
-tree->insertNode(1);
-tree->insertNode(3);
-tree->insertNode(6);
-tree->insertNode(4);
-tree->insertNode(7);
-tree->insertNode(1);
-tree->printSearchTree(1);
